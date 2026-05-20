a7e49e9d89a95b0e86dc22ba735cd4f88a41c4f6
Jesús <26531094+jestov@users.noreply.github.com>
2026-05-19 21:51:24 -0600

    Update layout.tsx

--- src/app/studio/[[...tool]]/layout.tsx
+++ src/app/studio/[[...tool]]/layout.tsx
@@ -4,8 +4,8 @@ export const metadata = {
 
 export default function StudioLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body style={{ margin: 0 }}>{children}</body>
-    </html>
+    <div className="fixed inset-0 z-[9999] bg-white">
+      {children}
+    </div>
   );
 }
